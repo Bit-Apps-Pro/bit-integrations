c8c45a10066a5474cc15cef3309fd4b4867b3c05
FahimSakib <fsakib991@gmail.com>
2024-04-21 16:50:02 +0600

    Update FluentBookingController.php

--- includes/Triggers/FluentBooking/FluentBookingController.php
+++ includes/Triggers/FluentBooking/FluentBookingController.php
@@ -167,6 +167,11 @@ final class FluentBookingController
         $bookingArray = $booking->toArray();
         unset($bookingArray['calendar_event']);
 
+        error_log(print_r($customFieldsData, true));
+        error_log(print_r($bookingArray, true));
+
+        // die;
+
         $form_id  = $bookingArray['event_id'];
         $formData = [];
 
