41fd71e6219341a750382772d9b2a4078ebac278
Md. Abed Hossen <95869988+Md-Abed-Hossen@users.noreply.github.com>
2025-08-20 16:15:12 +0600

    chore: update logo

--- frontend-dev/src/components/AllIntegrations/Line/Line.jsx
+++ frontend-dev/src/components/AllIntegrations/Line/Line.jsx
@@ -64,7 +64,7 @@ function Line({ formFields, setFlow, flow, allIntegURL }) {
       }
     }, 300)
 
-    if (val === 3 && (lineConf?.name || isNextButtonEnabled())) {
+    if (val === 3 && !(lineConf?.name || isNextButtonEnabled())) {
       const messages = getLineValidationMessages(lineConf)
       setSnackbar({
         show: true,
